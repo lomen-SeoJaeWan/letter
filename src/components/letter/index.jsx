7a6fe840304c1a3fb52e482b3d806dd222b60eaf
lomen-SeoJaeWan <sjw7324@lomen.kr>
2024-03-20 09:00:36 +0900

    feat: letter 페이지 완료

--- src/components/letter/index.jsx
+++ src/components/letter/index.jsx
@@ -2,32 +2,33 @@ import "./index.css";
 
 const Letter = () => {
   return (
-    <div class="letter-image">
-      <div class="animated-mail">
-        <div class="back-fold"></div>
-        <div class="letter">
-          <div class="letter-border"></div>
-          <div class="letter-title"></div>
-          <div class="letter-context"></div>
-          <div class="letter-context-body">
+    <div className={`letter-image`}>
+      <div className="animated-mail">
+        <div className="back-fold"></div>
+        <div className="letter">
+          <div className="letter-border"></div>
+          <div className="letter-title"></div>
+          <div className="letter-context"></div>
+          <div className="letter-context-body">
             <p>
               <strong>
                 Dear,
                 <br />
-                Ram Avtar
+                여러분
               </strong>
-              <br /> you are invited BPCON 2023
+              <br /> 이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를
+              돌면서...
             </p>
           </div>
-          <div class="letter-stamp">
-            <div class="letter-stamp-inner"></div>
+          <div className="letter-stamp">
+            <div className="letter-stamp-inner"></div>
           </div>
         </div>
-        <div class="top-fold"></div>
-        <div class="body"></div>
-        <div class="left-fold"></div>
+        <div className="top-fold"></div>
+        <div className="body"></div>
+        <div className="left-fold"></div>
       </div>
-      <div class="shadow"></div>
+      <div className="shadow"></div>
     </div>
   );
 };
